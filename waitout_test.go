a334c8caa38e604d8deab9a0f0f43b6b31a18b0e
Naitik Shah <n@daaku.org>
2013-11-15 15:12:37 -0800

    initial commit

---
+++ waitout_test.go
@@ -0,0 +1,87 @@
+package waitout_test
+
+import (
+	"testing"
+
+	"github.com/ParsePlatform/go.waitout"
+)
+
+func TestWait(t *testing.T) {
+	t.Parallel()
+	expected := []byte("42")
+	w := waitout.New(expected)
+	sent := make(chan struct{})
+	go func() {
+		defer close(sent)
+		w.Write(expected)
+	}()
+	w.Wait()
+	<-sent
+}
+
+func TestWaitSplit(t *testing.T) {
+	t.Parallel()
+	expected := []byte("42")
+	w := waitout.New(expected)
+	sent := make(chan struct{})
+	go func() {
+		defer close(sent)
+		w.Write([]byte("4"))
+		w.Write([]byte("2"))
+	}()
+	w.Wait()
+	<-sent
+}
+
+func TestTwice(t *testing.T) {
+	t.Parallel()
+	expected := []byte("42")
+	w := waitout.New(expected)
+	sent := make(chan struct{})
+	go func() {
+		defer close(sent)
+		w.Write(expected)
+		w.Write(expected)
+	}()
+	w.Wait()
+	<-sent
+}
+
+func TestManyInParallel(t *testing.T) {
+	t.Parallel()
+	const count = 100
+	expected := []byte("42")
+	w := waitout.New(expected)
+
+	event := make(chan bool)
+	for i := 0; i < count; i++ {
+		go func() {
+			if r := recover(); r != nil {
+				t.Fatal(r)
+			}
+			w.Write(expected)
+			event <- true
+		}()
+	}
+
+	for i := 0; i < count; i++ {
+		go func() {
+			if r := recover(); r != nil {
+				t.Fatal(r)
+			}
+			w.Wait()
+			event <- true
+		}()
+	}
+
+	done := make(chan struct{})
+	go func() {
+		defer close(done)
+		for i := 0; i < count*2; i++ {
+			<-event
+		}
+	}()
+
+	<-done
+	w.Wait()
+}
